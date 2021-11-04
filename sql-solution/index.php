f9182157b727afae4588d7aa0f24f304169c8b20
NellySP <89972433+NellySP@users.noreply.github.com>
2021-11-04 13:06:59 +0100

    added files for webpage

---
+++ sql-solution/index.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Solution</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="content">
+            <form action="post.php" method="post">
+                <div>
+                    <label for="name">Solution</label>
+                    <input type="name" name="name">
+                </div>
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    </div>
+
+</body>
+
+</html>
